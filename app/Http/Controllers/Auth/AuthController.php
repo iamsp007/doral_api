2de3ceadef1a4c38f843dac192b61ad4e67d52d7
Nikunj Kansara <nikunj.verve@gmail.com>
2020-11-18 13:26:57 +0530

    Made chages into Login API to proper feedback

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,33 +16,52 @@ class AuthController extends Controller
     //
     public function login(Request $request)
     {
+        $data = array();
+        //validation
         $request->validate([
-            'email' => 'required|string|email',
+            'username' => 'required|string|email',
             'password' => 'required|string'
         ]);
 
-        $credentials = request(['email', 'password']);
-        // print_r($credentials);die;
-        if (!Auth::attempt($credentials))
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me)
-            $token->expires_at = Carbon::now()->addMinute(1);
-        $token->save();
-        $data=[
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'user' => $user,
-            'expires_at' => Carbon::parse(
-                $tokenResult->token->expires_at
-            )->toDateTimeString()
-        ];
-
-        return $this->generateResponse(true, 'Login Successfully!',$data);
+        try {
+            $login = json_decode($request->getContent(), true);
+            $username = $login['username'];
+            $password = $login['password'];
+            
+            $user = User::login($request);
+            // Check user exist into database or not   
+            if (!$user) {
+                throw new Exception("Login Fail, please check your credential");
+            }
+            // Check user password
+            if (!Hash::check($password, $user->password)) {
+                throw new Exception("Login Fail, pls check password");
+            }
+            $credentials = ['email' => $username, 'password' => $password, 'status' => 'active'];
+            // print_r($credentials);die;
+            if (!Auth::attempt($credentials)) {
+                throw new Exception("Login Fail, pls check email/password, Or check Account status");
+            }
+            $user = $request->user();
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+            if ($request->remember_me)
+                $token->expires_at = Carbon::now()->addMinute(1);
+            $token->save();
+            $data = [
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'user' => $user,
+                'expires_at' => Carbon::parse(
+                    $tokenResult->token->expires_at
+                )->toDateTimeString()
+            ];
+            return $this->generateResponse(true, 'Login Successfully!', $data);
+        } catch (\Exception $e) {
+            $status = false;
+            $message = $e->getMessage() . " " . $e->getLine();
+            return $this->generateResponse($status, $message, $data);
+        }
     }
 
     public function register(Request $request)
@@ -52,7 +72,7 @@ class AuthController extends Controller
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string',
             'dob' => 'required|date',
-            'phone' => 'required|numeric'            
+            'phone' => 'required|numeric'
         ]);
         $user = new User;
         $user->first_name = $request->fName;
@@ -63,7 +83,7 @@ class AuthController extends Controller
         $user->phone = $request->phone;
         $user->save();
 
-        return $this->generateResponse(true, 'Login Successfully!',[
+        return $this->generateResponse(true, 'Login Successfully!', [
             'message' => 'Successfully created user!'
         ]);
     }
@@ -72,7 +92,8 @@ class AuthController extends Controller
     {
         $request->user()->token()->revoke();
         return response()->json([
-            'message' => 'Successfully logged out']);
+            'message' => 'Successfully logged out'
+        ]);
     }
 
     public function user(Request $request)
