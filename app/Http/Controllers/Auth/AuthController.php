a08c570b732e2601471326b3679ce8e7efbbfbe6
Sunil Karmur <sunil.k@lipitech.in>
2020-12-14 11:21:13 +0530

    authcontroller added

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -75,7 +75,7 @@ class AuthController extends Controller
         $user->password = Hash::make($request->password);
         $user->dob = $request->dob;
         $user->phone = $request->phone;
-        $user->status = 1;
+        $user->status = '1';
         //        $user->hasPermissionTo('Create', 'web');
         $user->assignRole($request->type)->syncPermissions(Permission::all());
         if ($user->save()){
