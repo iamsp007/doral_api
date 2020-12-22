03ddb5080a1c98a3308bd68ef466aaeeb6c31a06
mayank Abiyani <mayank@ainfosolutions.com>
2020-12-22 17:46:14 +0530

    Update in Patient Occupaational

--- app/Imports/BulkOccupationalImport.php
+++ app/Imports/BulkOccupationalImport.php
@@ -37,24 +37,25 @@ class BulkOccupationalImport implements ToModel,WithHeadingRow
     
     public function model(array $row)
     {
+
         try {
             $user = new User;
             $user->first_name = isset($row['First Name']) ? $row['First Name'] : NULL;
             $user->last_name = isset($row['Last Name']) ? $row['Last Name'] : NULL;
-            $user->email = isset($row['email']) ? $row['email'] : NULL;
+            $user->email = isset($row['Email']) ? $row['Email'] : NULL;
             $user->password = Hash::make('test123');
+            $user->dob = isset($row['Date of Birth']) ? date('Y-m-d', strtotime($row['Date of Birth'])) : NULL;
             //$user->dob = '2020-10-10';
-            if(isset($row['Home Phone']) && !empty($row['Home Phone'])) {
-                $user->phone = str_replace('-', '', $row['Home Phone']);
-            } elseif(isset($row['Phone2']) && !empty($row['Phone2'])) {
+            if(isset($row['Phone2']) && !empty($row['Phone2'])) {
                 $user->phone = str_replace('-', '', $row['Phone2']);
-            } elseif(isset($row['Home Phone2']) && !empty($row['Home Phone2'])) {
-                $user->phone = str_replace('-', '', $row['Home Phone2']);
+            } elseif(isset($row['Emergency1 Phone1']) && !empty($row['Emergency1 Phone1'])) {
+                $user->phone = str_replace('-', '', $row['Emergency1 Phone1']);
             }
             $user->assignRole('patient')->syncPermissions(Permission::all());
             $user->save();
-
+            
             $userId = $user->id;
+            //dd($userId);
             return new PatientOccupational([
                 'referral_id' => $this->referral_id,
                 'service_id' => $this->service_id,
@@ -62,19 +63,19 @@ class BulkOccupationalImport implements ToModel,WithHeadingRow
                 'form_id' => $this->form_id,
                 'first_name'     => $row['First Name'],
                 'last_name'    => $row['Last Name'],
+                'middle_name'    => isset($row['Middle Name']) ? $row['Middle Name'] : NULL,
                 'caregiver_code' => isset($row['Caregiver Code']) ? $row['Caregiver Code'] : NULL,
                 'ssn' => isset($row['SSN']) ? $row['SSN'] : NULL,
-                'medicaid_number' => isset($row['Medicaid Number']) ? $row['Medicaid Number'] : NULL,
-                'medicare_number' => isset($row['Medicare Number']) ? $row['Medicare Number'] : NULL,
-                'address_1' => isset($row['Address1']) ? $row['Address1'] : NULL,
-                'address_2' => isset($row['Address2']) ? $row['Address2'] : NULL,
-                'city' => isset($row['City']) ? $row['City'] : NULL,
-                'state' => isset($row['State']) ? $row['State'] : NULL,
-                'Zip' => isset($row['State']) ? $row['Zip Code'] : NULL,
-                'phone1' => isset($row['Home Phone']) ? $row['Home Phone'] : NULL,
-                'phone2' => isset($row['Home Phone2']) ? $row['Home Phone2'] : NULL,
+                'gender' => isset($row['Gender']) ? $row['Gender'] : NULL,
+                'dob' => isset($row['Date of Birth']) ? date('Y-m-d', strtotime($row['Date of Birth'])) : NULL,
+                'address_1' => isset($row['Street1']) ? $row['Street1'] : NULL,
+                'phone1' => isset($row['Emergency1 Phone1']) ? $row['Emergency1 Phone1'] : NULL,
+                'phone2' => isset($row['Phone2']) ? $row['Phone2'] : NULL,
+                'email' => isset($row['Email']) ? $row['Email'] : NULL,
                 'user_id' => $userId
             ]);
-        } catch(Exception $e) {}
+        } catch(Exception $e) {
+            //dd($e->getMessage());
+        }
     }
 }
