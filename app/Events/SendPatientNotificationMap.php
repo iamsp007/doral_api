33142795895664067174b6961f06f56de79d7448
sunil-k <sunil.ahir@globalgarner.com>
2021-01-22 17:54:20 +0530

    notification function changes

--- app/Events/SendPatientNotificationMap.php
+++ app/Events/SendPatientNotificationMap.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Helpers\Helper;
 use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
@@ -27,47 +28,8 @@ class SendPatientNotificationMap
             $message="Clinician RoadL Route";
             $title="Clinician RoadL Route";
             $token=$user->device_token;
-            $this->sendPushNotification($token,$title,$message,$data);
+            $helper = new Helper();
+            $helper->sendNotification($token,$title,$data);
         }
-    }
-
-    private function sendPushNotification( $token,$title,$message,$data ) {
-
-        // Set POST variables
-
-        $path_to_fcm='https://fcm.googleapis.com/fcm/send';
-        $server_key=env('FIREBASE_CREDENTIALS');
-        $key= $token;
-        $headers=array(
-            'Authorization:key='.$server_key,
-            'Content-Type:application/json'
-        );
-//        $fields=array(
-//            'message'=>array(
-//                'token'=>$key,
-//                'data'=>$data
-//            )
-//        );
-        $fields=array(
-            'to'=>$key,
-            'notification'=>array(
-                'title'=>$title,
-                'body'=>$data
-            ),
-        );
-
-        $payload=json_encode($fields);
-        $curl_session=curl_init();
-        curl_setopt($curl_session,CURLOPT_URL,$path_to_fcm);
-        curl_setopt($curl_session,CURLOPT_POST,true);
-        curl_setopt($curl_session,CURLOPT_HTTPHEADER,$headers);
-        curl_setopt($curl_session,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($curl_session,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($curl_session,CURLOPT_IPRESOLVE,CURL_IPRESOLVE_V4);
-        curl_setopt($curl_session,CURLOPT_POSTFIELDS,$payload);
-
-        $result=curl_exec($curl_session);
-        \Log::info($result);
-        curl_close($curl_session);
     }
 }
