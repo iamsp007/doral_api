8019afb78fc1256c0495044b5c713efbab182c9f
manishak <manishak@hcbspro.com>
2021-02-04 18:47:39 +0530

    Change:labReportType seeder

--- database/seeders/LabReportTypeSeeder.php
+++ database/seeders/LabReportTypeSeeder.php
@@ -14,11 +14,95 @@ class LabReportTypeSeeder extends Seeder
      */
     public function run()
     {
-        $names = array('PPD/QuantiFERON', 'TB Screen', 'Rubeola', 'Rubeola MMR1', 'Rubeola MMR2', 'Rubella', 'Rubella MMR', 'Facemask Provided', 'Drug Screen', 'Annual Health Assessment');
-        foreach ($names as $name) {
-            $labReportTypeModel = new LabReportType();
-            $labReportTypeModel->name = $name;
-            $labReportTypeModel->save();
-        }
+        LabReportType::truncate();
+
+        $types =  [
+            [
+                'name' => 'TB Screen',
+                'sequence' => 1,
+                'status' => 1,
+                'parent_id' => NULL,
+            ],
+            [
+                'name' => 'PPD',
+                'sequence' => 1,
+                'status' => 1,
+                'parent_id' => 1,
+            ],
+            [
+                'name' => 'QuantiFERON',
+                'sequence' => 2,
+                'status' => 1,
+                'parent_id' => 1,
+            ],
+            [
+                'name' => 'Chest X-Ray',
+                'sequence' => 3,
+                'status' => 1,
+                'parent_id' => 1,
+            ],
+            [
+                'name' => 'TB(Gold)',
+                'sequence' => 4,
+                'status' => 1,
+                'parent_id' => 1,
+            ],
+            [
+                'name' => 'TB(Gold Plus)',
+                'sequence' => 5,
+                'status' => 1,
+                'parent_id' => 1,
+            ],
+            [
+                'name' => 'Immunization',
+                'sequence' => 2,
+                'status' => 1,
+                'parent_id' => NULL,
+            ],
+            [
+                'name' => 'Rubella',
+                'sequence' => 1,
+                'status' => 1,
+                'parent_id' => 2,
+            ],
+            [
+                'name' => 'Rubeola',
+                'sequence' => 2,
+                'status' => 1,
+                'parent_id' => 2,
+            ],
+            [
+                'name' => 'Rubella MMR',
+                'sequence' => 3,
+                'status' => 1,
+                'parent_id' => 2,
+            ],
+            [
+                'name' => 'Rubeola MMR',
+                'sequence' => 4,
+                'status' => 1,
+                'parent_id' => 2,
+            ],
+            [
+                'name' => 'Drug Screen',
+                'sequence' => 3,
+                'status' => 1,
+                'parent_id' => NULL,
+            ],
+            [
+                'name' => 'Drug Screen',
+                'sequence' => 1,
+                'status' => 1,
+                'parent_id' => 3,
+            ],
+            [
+                'name' => 'Drug Test',
+                'sequence' => 2,
+                'status' => 1,
+                'parent_id' => 3,
+            ],
+        ];
+
+        LabReportType::insert($types);
     }
 }
