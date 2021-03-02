678f542de072baa88686e972740f756996ddd925
manishak <manishak@hcbspro.com>
2021-03-02 16:01:41 +0530

    Update:status enum type

---
+++ database/migrations/2021_03_02_095116_update_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            DB::statement("ALTER TABLE users CHANGE COLUMN status status ENUM('0', '1', '2', '3', '4') NOT NULL DEFAULT '0'");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
