c08eee2860843f733a0853238ddb8a09943e0c26
Sunil Karmur <sunil.k@lipitech.in>
2021-03-13 00:53:48 +0530

    modal changes

--- app/Models/Referral.php
+++ app/Models/Referral.php
@@ -10,17 +10,31 @@ class Referral extends Model
     use HasFactory;
 
 
+
     /**
      * Get the user's Date Of Birth.
      *
      * @return string
      */
-    public function getIconAttribute()
+    public function getIconAttribute($value)
     {
-        if (isset($this->icon) && !empty($this->icon)) {
-            return env('WEB_URL').'assets/icon/'. $this->icon;
-        } else {
-            return env('WEB_URL').'assets/icon/Clinician Request.png';
+        $icon=env('WEB_URL').'assets/icon/Clinician Request.png';
+        if ($value) {
+            $icon=env('WEB_URL').'assets/icon/'. $value;
         }
+        return $icon;
+    }
+    /**
+     * Get the user's Date Of Birth.
+     *
+     * @return string
+     */
+    public function getColorAttribute($value)
+    {
+        $color='green';
+        if ($value) {
+            $color=$value;
+        }
+        return $color;
     }
 }
