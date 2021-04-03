8f6218dc317ae220497e94f85924d5d633579462
manishak <manishak@hcbspro.com>
2021-04-03 11:24:43 +0530

    Change:demographic and emergency contact detail table

---
+++ database/migrations/2021_04_03_004141_add_notification_demographi.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotificationDemographi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('demographics', function (Blueprint $table) {
+            $table->string('notification')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('demographics', function (Blueprint $table) {
+            //
+        });
+    }
+}
