813e0d5da6d94aa0231b45af0afe45c3c37a5bd3
manishak <manishak@hcbspro.com>
2021-07-13 17:17:46 +0530

    Added:dieses

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SymptomsMaster;
 use App\Models\Test;
 use Illuminate\Http\Request;
+use Illuminate\Log\Logger;
+use Illuminate\Support\Facades\Log;
 
 class TestController extends Controller
 {
@@ -33,13 +36,16 @@ class TestController extends Controller
     public function getMultiTest(Request $request)
     {
         $input = $request->all();
-        $categories = '';
-        if (isset($input['category_id'])) {
-            $categories = Test::with('subTestName')->whereIn('category_id',$input['category_id'])
+        $data = '';
+
+        if (isset($input['patient_roles_name']) && isset($input['category_id'])) {
+            $data = SymptomsMaster::where('dieser_id',$input['category_id'])->where('status',1)->get();
+        } else if (isset($input['category_id'])) {
+            $data = Test::with('subTestName')->whereIn('category_id',$input['category_id'])
             ->where('status',"1")
             ->get();
         }
-       
-        return $this->generateResponse(true,'Test List',$categories,200);
+
+        return $this->generateResponse(true,'Test List',$data,200);
     }
 }
