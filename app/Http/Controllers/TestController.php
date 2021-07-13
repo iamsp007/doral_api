e798a2972226ebab933693eb033c5b7508cd5a6f
manishak <manishak@hcbspro.com>
2021-07-13 17:22:36 +0530

    REmove:unused code

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\SymptomsMaster;
 use App\Models\Test;
 use Illuminate\Http\Request;
-use Illuminate\Log\Logger;
-use Illuminate\Support\Facades\Log;
 
 class TestController extends Controller
 {
