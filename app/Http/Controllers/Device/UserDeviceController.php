f558b71d8ca31934fb937e76a28b7f6d7d9dd2e0
hcbstech <shashikant@hcbspro.com>
2021-09-27 13:17:46 +0000

    live

--- app/Http/Controllers/Device/UserDeviceController.php
+++ app/Http/Controllers/Device/UserDeviceController.php
@@ -17,26 +17,25 @@ class UserDeviceController extends Controller
 
             $apiKey = ApiKey::where([['name', '=', $input['AppName']],['key', '=', $input['AppKey']],['secret', '=', $input['AppSecret']]])->first();
             if ($apiKey) {
-                $userDevice = UserDevice::with(['demographic' => function ($q) use($apiKey) {
-                    $q->where('company_id', $apiKey->company_id);
-                }])->where([['id', '=', $input['user_device_id']],['device_type', '=', $input['device_type']]])->first();
-               
-                if($userDevice) {
-                    if ($userDevice->demographic != '') {
+//                $userDevice = UserDevice::with(['demographic' => function ($q) use($apiKey) {
+//                    $q->where('company_id', $apiKey->company_id);
+//                }])->where([['id', '=', $input['user_id']],['device_type', '=', $input['device_type']]])->first();
+//               
+//                if($userDevice) {
+//                    if ($userDevice->demographic != '') {
                         $userDeviceLog = new UserDeviceLog();
-
-                        $userDeviceLog->user_device_id = $userDevice->id;
+                        $userDeviceLog->user_id = $input['user_id'];
+                        $userDeviceLog->device_type = $input['device_type'];
                         $userDeviceLog->value = $input['value'];
                         $userDeviceLog->reading_time = $input['datetime'];
-
                         $userDeviceLog->save();
                         return $this->generateResponse(true,'User device log added successfully.',$userDeviceLog,200);
-                    } else {
-                        return $this->generateResponse(true,'Unauthenticated',null,401);
-                    }
-                } else {
-                    return $this->generateResponse(true,'User device not found.',null,200);
-                }
+//                    } else {
+//                        return $this->generateResponse(true,'Unauthenticated',null,401);
+//                    }
+//                } else {
+//                    return $this->generateResponse(true,'User device not found.',null,200);
+//                }
             } else {
                 return $this->generateResponse(true,'Unauthenticated',null,401);
             }
