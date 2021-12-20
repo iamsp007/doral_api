32ecf0722aac911e5036acd1b3b7cd4cfbd95c70
manishahcbs <manishak@hcbspro.com>
2021-12-20 15:31:25 +0530

    added:scraping data

--- app/Jobs/ScrapingData.php
+++ app/Jobs/ScrapingData.php
@@ -2,6 +2,9 @@
 
 namespace App\Jobs;
 
+use App\Models\NursePractitionerUsers;
+use App\Models\PhysicianAssistantUsers;
+use App\Models\PhysicianUsers;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -32,8 +35,46 @@ class ScrapingData implements ShouldQueue
      */
     public function handle()
     {
-        $users = DB::connection('mysql2');
-        $users->table('user');
-        dd($users);
+        //designation_name = 1(NP), 4(PA), 9(P)
+        if ($this->input['designation_id'] === '9') {
+            PhysicianUsers::create([
+                'category_id' => '1',
+                'speciality_id' => '2',
+                'first_name' => $this->input['first_name'],
+                'last_name' => $this->input['last_name'],
+                'ssn_no' => $this->input['ssn_no'],
+                'dea_no' => $this->input['dea_no'],
+                'zip_code' => $this->input['zip_code'],
+                'expire_month' => $this->input['expire_month'],
+                'expire_year' => $this->input['expire_year'],
+                'date_of_birth' => $this->input['date_of_birth'],
+            ]);
+        } else if ($this->input['designation_id'] === '1') {
+            NursePractitionerUsers::create([
+                'category_id' => '2',
+                'speciality_id' => '1',
+                'first_name' => $this->input['first_name'],
+                'last_name' => $this->input['last_name'],
+                'ssn_no' => $this->input['ssn_no'],
+                'dea_no' => $this->input['dea_no'],
+                'zip_code' => $this->input['zip_code'],
+                'expire_month' => $this->input['expire_month'],
+                'expire_year' => $this->input['expire_year'],
+                'date_of_birth' => $this->input['date_of_birth'],
+            ]);
+        } else if ($this->input['designation_id'] === '4') {
+            PhysicianAssistantUsers::create([
+                'category_id' => '3',
+                'speciality_id' => '2',
+                'first_name' => $this->input['first_name'],
+                'last_name' => $this->input['last_name'],
+                'ssn_no' => $this->input['ssn_no'],
+                'dea_no' => $this->input['dea_no'],
+                'zip_code' => $this->input['zip_code'],
+                'expire_month' => $this->input['expire_month'],
+                'expire_year' => $this->input['expire_year'],
+                'date_of_birth' => $this->input['date_of_birth'],
+            ]);
+        } 
     }
 }
